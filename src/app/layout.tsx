d0207df6d44dc38e094b7598a3739795df439e16
Alena Batitskaia <batickaya.a@gmail.com>
2024-11-21 14:58:23 +0000

    Suspence added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
-  title: "Public Holidays",
-  description: "Quickly find public holidays in your country",
+  title: 'Public Holidays',
+  description: 'Quickly find public holidays in your country',
 };
 
 export default function RootLayout({
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Suspense>{children}</Suspense>
+      </body>
     </html>
   );
 }
